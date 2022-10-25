db4ea97a4cb9d1049832d5a02107b366b8e01cc8
Eric Chen <eschen@cpp.edu>
2022-10-24 22:16:12 -0700

    advanced webview added

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.work.*
 import com.example.photogallery.api.FlickrApi
@@ -106,8 +107,11 @@ class PhotoGalleryFragment : Fragment() {
                     binding.photoGrid.adapter = PhotoListAdapter(
                         state.images
                     ){photoPageUri ->
-                        val intent = Intent(Intent.ACTION_VIEW, photoPageUri)
-                        startActivity(intent)
+                        findNavController().navigate(
+                            PhotoGalleryFragmentDirections.showPhoto(
+                                photoPageUri
+                            )
+                        )
                     }
                     searchView?.setQuery(state.query, false)
                     updatePollingState(state.isPolling)
